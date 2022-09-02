833557dcabc93e15e2ee2f4591c6aabd8b515793
Helen Lehrer <hlehrer@outlook.com>
2022-09-02 09:23:44 -0700

    Pass 2nd test

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -8,6 +8,10 @@ export class Age {
   }
 
   calculatePlanetaryAges() {
-    
+    this.mercury = Math.round(this.mercury + (this.earth / .24));
+    this.venus = Math.round(this.venus + (this.earth / .62));
+    this.mars = Math.round(this.mars + (this.earth / 1.88));
+    this.jupiter = Math.round(this.jupiter + (this.earth / 11.86));
+    return this
   }
 }
