3900914db796c0756cbc27645441aee0c20ba8bd
Helen Lehrer <hlehrer@outlook.com>
2022-09-02 17:33:43 -0700

    WIP UI

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { GalacticCalculator } from './js/calculator.js';
+import { LifeExpectancy } from './js/calculator.js';
+
+const handleFormSubmission = (event) => {
+event.preventDefault();
+}
+
+const outputPlanetaryAges = () => {
+  let ageInput = document.getElementById("age-input");
+}
+
+let form = document.getElementById("calculatorInput");
+form.addEventListener("submit", handleFormSubmission);
